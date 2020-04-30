3d5ee34ef0849f749673d8fcfa96808da491812d
NBSanchez <Nbsanchez@miuandes.cl>
2020-04-30 17:36:42 -0400

    Clases-Atributo-Critica-Rating

---
+++ Proyecto-Grupo 6/Proyecto-Grupo 6/Atributo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Grupo_6
+{
+    class Atributos
+    {
+        protected string UserName;
+
+        public void SetUser(string UserName)
+        {
+            this.UserName = UserName;
+        }
+        public string GetUser()
+        {
+            return UserName;
+        }
+
+    }
+
+}
---
+++ Proyecto-Grupo 6/Proyecto-Grupo 6/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Grupo_6
+{
+    class Rating : Atributos
+    {
+        private int ValorRating;
+
+        public void SetRating(int ValorRating)
+        {
+            this.ValorRating = ValorRating;
+        }
+        public int GetRating()
+        {
+            return ValorRating;
+        }
+    }
+
+}
